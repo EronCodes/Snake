6b26f5d1b92f933e93e0e6f8764c178347c1adb8
Ivo Ziesche <ivo.ziesche@web.de>
2018-04-05 17:19:51 +0200

    Initial commit

---
+++ objekte.js
@@ -0,0 +1,26 @@
+var intervalId;
+
+var feldBreite = 20;
+var feldHoehe = 10;
+
+var zeit1;
+
+var geschwindigkeit;
+
+var pflaumenPoses = [];
+
+var snake = {
+  //Die besetzten Felder werden vom Schwanz (Anfang des Arrays) bis zum Kopf (Ende des Arrays) geordnet
+  besetzteFelder: [],
+
+  fruchtPos: [],
+  gefressen: false,
+
+  /*
+    0: oben
+    1: rechts
+    2: unten
+    3: links
+  */
+    orientation: 1
+};
---
+++ script.js
@@ -0,0 +1,314 @@
+document.onkeydown = tasteGedrueckt;
+document.getElementById("startButton").addEventListener("click", spielStarten);
+document.getElementById("expert").addEventListener("click", openExpert);
+document.getElementById("close").addEventListener("click", closeExpert);
+
+function closeExpert() {
+  document.getElementById("gameSettings").style.display = "none";
+}
+
+function openExpert() {
+  document.getElementById("gameSettings").style.display = "block";
+}
+
+function spielStarten() {
+  document.getElementById("buttonDiv").style.display = "none";
+  var feld = document.getElementById("spielfeld");
+
+  var congratulation = document.getElementById("congratulation");
+  congratulation.style.display = "none";
+
+  while(feld.hasChildNodes())
+    feld.removeChild(feld.firstChild);
+
+  feldBreite = document.getElementById("breite_start").value;
+  feldHoehe = document.getElementById("hoehe_start").value;
+
+  snake.besetzteFelder = [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0]
+  ];
+
+  pflaumeErzeugen();
+  fruchtErzeugen();
+
+  var aktZeit = new Date();
+  zeit1 = aktZeit.getTime();
+
+  tabelleErstellen();
+
+  document.getElementById("startDiv").style.display = "none";
+  document.getElementById("endDiv").style.display = "none";
+  document.getElementById("gameSettings").style.display = "none";
+
+  snake.orientation = 1;
+
+  snake.gefressen = false;
+
+  var zellen = document.getElementsByTagName("td");
+  for(var i=0; i<zellen.length; i++) {
+    zellen[i].style.backgroundColor = "#505050";
+  }
+
+  geschwindigkeit = document.getElementById("geschwindigkeit_start").value;
+
+  intervalId = setInterval(setupBewegungswerte, geschwindigkeit);
+}
+
+function fruchtErzeugen() {
+  snake.fruchtPos[0] = Math.floor(Math.random()*feldBreite);
+  snake.fruchtPos[1] = Math.floor(Math.random()*feldHoehe);
+
+  for(var t=0; t<pflaumenPoses.length; t++) {
+    //Liegt der Apfel auf einer Pflaume?
+    if((snake.fruchtPos[0] == pflaumenPoses[t][0]) && (snake.fruchtPos[1] == pflaumenPoses[t][1])) {
+      fruchtErzeugen();
+    }
+  }
+}
+
+function pflaumeErzeugen() {
+  pflaumenPoses = [];
+  var number = document.getElementById("anzPflaumen_start").value;
+
+  for (var k = 0; k < number; k++) {
+    pflaumenPoses[k] = [Math.floor(Math.random()*feldBreite), Math.floor(Math.random()*feldHoehe)];
+    //liegt eine Pflaume auf einer Pflaume?
+    for (var t = 0; t < pflaumenPoses.length-1; t++) {
+      if (pflaumenPoses[pflaumenPoses.length-1][0] == pflaumenPoses[t][0] && pflaumenPoses[pflaumenPoses.length-1][1] == pflaumenPoses[t][1]) {
+        pflaumeErzeugen();
+        return;
+      }
+    }
+  }
+
+  for(var u=0; u<pflaumenPoses.length; u++) {
+    for (var i = 0; i < snake.besetzteFelder.length; i++) {
+      //Liegt eine Pflaume auf Snake?
+      if((snake.besetzteFelder[i][0] == pflaumenPoses[u][0]) && (snake.besetzteFelder[i][1] == pflaumenPoses[u][1])) {
+        pflaumeErzeugen();
+        return;
+      }
+    }
+    if (pflaumenPoses[u][0] == 4 && pflaumenPoses[u][1] == 0) {
+      pflaumeErzeugen();
+      return;
+    }
+  }
+}
+
+function tabelleErstellen() {
+  var tabelle = document.getElementById("spielfeld");
+
+  for(var i=0; i<feldHoehe; i++) {
+    var tr = document.createElement("tr");
+    tabelle.appendChild(tr);
+  }
+
+  var reihen = document.getElementsByTagName("tr");
+  for(var k=0; k<feldHoehe; k++) {
+    for(var u=0; u<feldBreite; u++) {
+      var td = document.createElement("td");
+      td.setAttribute("id", u + "_" + k);
+      reihen[k].appendChild(td);
+    }
+  }
+
+  var body = document.getElementById("body");
+  body.appendChild(tabelle);
+}
+
+function setupGrafik() {
+  var zellen = document.getElementsByTagName("td");
+  for(var i=0; i<zellen.length; i++) {
+    zellen[i].style.backgroundColor = "#0a0";
+    zellen[i].style.backgroundImage = "none";
+  }
+
+  var fruchtFeld = document.getElementById(snake.fruchtPos[0] + "_" + snake.fruchtPos[1]);
+  fruchtFeld.style.backgroundImage = "url(images/apfel.jpg)";
+
+  for(var v=0; v<pflaumenPoses.length; v++) {
+    var pflaumenFeld = document.getElementById(pflaumenPoses[v][0] + "_" + pflaumenPoses[v][1]);
+    pflaumenFeld.style.backgroundImage = "url(images/pflaume.jpg)";
+  }
+
+  for (var k=0; k<snake.besetzteFelder.length; k++) {
+    var id = snake.besetzteFelder[k];
+    var feld = document.getElementById(id[0]+"_"+id[1]);
+    feld.style.backgroundColor = "#00f";
+
+    //Hat Snake einen Apfel gefressen?
+    if((snake.besetzteFelder[k][0] == snake.fruchtPos[0]) && (snake.besetzteFelder[k][1] == snake.fruchtPos[1])) {
+      snake.gefressen = true;
+      var apfel = document.getElementById(snake.fruchtPos[0] + "_" + snake.fruchtPos[1]);
+      apfel.style.backgroundImage = "none";
+      apfel.style.backgroundColor = "#00f";
+      fruchtErzeugen();
+    }
+  }
+
+  var kopfFeld = document.getElementById(snake.besetzteFelder[snake.besetzteFelder.length-1][0] + "_" + snake.besetzteFelder[snake.besetzteFelder.length-1][1]);
+  kopfFeld.style.backgroundImage = "url(images/kopf_" + snake.orientation + ".png)";
+}
+
+function tasteGedrueckt(event) {
+  event.cancelBubble = true;
+  event.returnValue = false;
+
+  switch (event.keyCode) {
+    case 37: //links
+      snake.orientation = 3;
+      break;
+    case 38: //oben
+      snake.orientation = 0;
+      break;
+    case 39: //rechts
+      snake.orientation = 1;
+      break;
+    case 40: //unten
+      snake.orientation = 2;
+      break;
+  }
+
+  return event.returnValue;
+}
+
+function setupBewegungswerte() {
+  if(snake.gefressen == false) {
+    //Schwanzelement entfernen
+    snake.besetzteFelder.shift();
+  }else {
+    snake.gefressen = false;
+  }
+
+  //temporäres Array, also Kopie von snake.besetzteFelder, erzeugen
+  var pos = [];
+  for (var z in snake.besetzteFelder) {
+    pos.push(snake.besetzteFelder[z]);
+  }
+
+  var kopfPos = pos.pop();
+
+  var ende = [];
+  for (var r in kopfPos) {
+    ende.push(kopfPos[r]);
+  }
+
+  switch (snake.orientation) {
+    case 3: //links
+      ende[0]--;
+      break;
+    case 0: //oben
+      ende[1]--;
+      break;
+    case 1: //rechts
+      //Schwanzelement entfernen
+      ende[0]++;
+      break;
+    case 2: //unten
+      ende[1]++;
+      break;
+  }
+  snake.besetzteFelder.push(ende);
+
+  if(todesPruefung())
+    spielBeenden();
+  else
+    setupGrafik();
+}
+
+function todesPruefung() {
+  var tmpFelder = [];
+  for (var i in snake.besetzteFelder) {
+    tmpFelder[i] = snake.besetzteFelder[i];
+  }
+
+  //Snake auf Pflaume?
+  for(var t=0; t<pflaumenPoses.length; t++) {
+   if((tmpFelder[tmpFelder.length-1][0] == pflaumenPoses[t][0]) && (tmpFelder[tmpFelder.length-1][1] == pflaumenPoses[t][1]))
+     return true;
+  }
+
+  var kopf = tmpFelder[tmpFelder.length-1];
+
+  //Snake außerhalb der Welt?
+  if (kopf[0] > feldBreite-1 || kopf[0] < 0 || kopf[1] < 0 || kopf[1] > feldHoehe-1)
+    return true;
+
+  //Hat Snake sich selbst gefressen?
+  for(var z=0; z<tmpFelder.length-1; z++) {
+    if(kopf[0] == tmpFelder[z][0] && kopf[1] == tmpFelder[z][1])
+      return true;
+  }
+
+  return false;
+}
+
+function spielBeenden() {
+  clearInterval(intervalId);
+
+  var gameValues;
+
+  document.getElementById("endDiv").style.display = "block";
+  var button = document.getElementById("buttonDiv");
+  button.style.display = "block";
+  button.style.margin = "140px 0 0 -98.5px";
+  document.getElementById("endButton").addEventListener("click", spielStarten);
+
+  var aktZeit2 = new Date();
+  var zeit2 = aktZeit2.getTime();
+  var spieldauer = (zeit2 - zeit1)/1000;
+  var statistik = document.getElementById("statistik");
+
+  if (window["localStorage"]) {
+    var wertissimo = [];
+
+    for (var i = 0; i < localStorage.length; i++) {
+      wertissimo[i] = localStorage.getItem(localStorage.key(i));
+    }
+
+    var aktZeit = new Date();
+    var zeit = aktZeit.getTime();
+    localStorage.setItem(zeit, snake.besetzteFelder.length-4);
+
+    var platzierung = calcPlace(wertissimo, snake.besetzteFelder.length-4);
+    var highscore = calcHighscore(wertissimo);
+
+    //Erster Platz?
+    if (platzierung == 1) {
+      var congratulation = document.getElementById("congratulation");
+      congratulation.style.display = "block";
+    }
+
+    gameValues = snake.besetzteFelder.length-4 + " Äpfel" + "<br>" + spieldauer + " Sekunden<br>" + platzierung + " Platz insgesamt<br>" + highscore + " Äpfel Highscore";
+  } else {
+    gameValues = snake.besetzteFelder.length-4 + " Äpfel<br>" + spieldauer + " Sekunden";
+  }
+
+  statistik.innerHTML = gameValues;
+}
+
+function calcPlace(werte, aktWert) {
+  var platz = 0;
+  for(var i=0; i<werte.length; i++) {
+    if(aktWert > werte[i])
+      platz++;
+  }
+
+  var highscore = werte.length - platz + 1;
+  return highscore;
+}
+
+function calcHighscore(wertissimo) {
+  var bigger = 0;
+
+  for (var i = 0; i < wertissimo.length; i++) {
+    if (parseInt(bigger) < parseInt(wertissimo[i]))
+      bigger = wertissimo[i];
+  }
+
+  return bigger;
+}
