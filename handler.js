fbada56f947c180c04e1665850d4eb552b7e050f
Ivo Ziesche <ivo.ziesche@web.de>
2018-04-24 17:59:07 +0200

    Split the script.js file in the files core.js, there are important functions setupBewrgungsWerte(), etc., extras.js and handler.js

---
+++ handler.js
@@ -0,0 +1,5 @@
+document.onkeydown = tasteGedrueckt;
+document.getElementById("startButton").addEventListener("click", spielStarten);
+document.getElementById("expert").addEventListener("click", openExpert);
+document.getElementById("close").addEventListener("click", closeExpert);
+document.getElementById("pause").addEventListener("click", pause);
