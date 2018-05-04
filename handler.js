c88efdae6dfe083e35d106b49fd1194dc40a8f37
Ivo Ziesche <ivo.ziesche@web.de>
2018-05-04 18:30:01 +0200

    Cancel the spltting in four files

--- handler.js
+++
@@ -1,5 +0,0 @@
-document.onkeydown = tasteGedrueckt;
-document.getElementById("startButton").addEventListener("click", spielStarten);
-document.getElementById("expert").addEventListener("click", openExpert);
-document.getElementById("close").addEventListener("click", closeExpert);
-document.getElementById("pause").addEventListener("click", pause);
